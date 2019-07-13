eeff9a87114e2e63d1b952d27b76cf2454ddee7f
bakiyildiz <37048315+bakiyildiz@users.noreply.github.com>
2019-07-13 14:43:32 +0300

    Initial commit

---
+++ src/sendmailservice/SendMailService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sendmailservice;
+
+/**
+ *
+ * @author Baki
+ */
+public class SendMailService {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        SendMailServiceIndex snd = new SendMailServiceIndex();
+        snd.setTitle("SendMailService");
+        snd.setVisible(true);
+    }
+    
+}
