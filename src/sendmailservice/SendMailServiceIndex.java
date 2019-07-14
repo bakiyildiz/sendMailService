62875bad7dbeede824d29f217f1429bfc3676fcd
bakiyildiz <37048315+bakiyildiz@users.noreply.github.com>
2019-07-15 00:52:58 +0300

    gmail geliştirmesi

--- src/sendmailservice/SendMailServiceIndex.java
+++ src/sendmailservice/SendMailServiceIndex.java
@@ -188,6 +188,7 @@ public class SendMailServiceIndex extends javax.swing.JFrame {
         props.put("mail.smtp.host", host);
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.starttls.enable", "true");
 
         Session session = Session.getDefaultInstance(props,  
         new javax.mail.Authenticator() {  
